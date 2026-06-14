8b36ad2747be7166b0d0db19972d97bc7563312e
sriramgoud889-creator <sriramgoud889@gmail.com>
2026-06-14 20:00:57 +0530

    Create script.js

---
+++ Task-3-Business-Website/script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log("Sri Sai Tiffin Center Website Loaded - Task 3 Complete");
