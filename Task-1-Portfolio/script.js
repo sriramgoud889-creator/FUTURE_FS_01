f3cb3a691cab40656c035e0bd62e229ecfd5a6fb
sriramgoud889-creator <sriramgoud889@gmail.com>
2026-06-14 18:05:23 +0530

    Create script.js

---
+++ Task-1-Portfolio/script.js
@@ -0,0 +1,15 @@
+// Portfolio Loaded
+console.log("Portfolio Loaded - Sriram Goud");
+
+// Smooth scroll for navbar links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
